3191953051b5b8e2b0b4e89cc0ff60e2f4821ea4
Florian TIAR <contact@tiar-florian.fr>
2016-08-27 18:03:29 +0200

    Properly enqueue the script

--- classes/main.php
+++ classes/main.php
@@ -20,6 +20,7 @@ class Main {
 
 	protected function init() {
 		add_action( 'init', array( $this, 'init_translations' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_marginotes_lib' ) );
 	}
 
 	/**
@@ -28,6 +29,21 @@ class Main {
 	public static function init_translations() {
 		// Load translations
 		load_plugin_textdomain( 'bea-wp-margin-notes', false, BEA_WPMN_DIR . 'languages' );
+	}
+
+	/**
+	 * Load margin notes script on singular tpl
+	 */
+	public function enqueue_marginotes_lib() {
+		$script_registration = wp_register_script( 'margin-notes', BEA_WPMN_DIR . 'assets/js/vendor/marginotes.js', array( 'jquery' ), false, true);
+		if ( false === $script_registration ) {
+			trigger_error( __( 'BEA WP Margin Notes : wp_register_script fails to register assets/js/vendor/marginotes.js', 'bea-wp-margin-notes' ) );
+		}
+
+		// Load the script only in singular tpl (pages, posts, cpts)
+		if ( is_singular() ) {
+			wp_enqueue_script( 'margin-notes' );
+		}
 	}
 
 }
