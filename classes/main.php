5fcbc17f05ceefc4f97131eca0499854bf6eff40
Florian TIAR <contact@tiar-florian.fr>
2016-08-27 22:23:05 +0200

    Enqueue script JS

--- classes/main.php
+++ classes/main.php
@@ -35,14 +35,20 @@ class Main {
 	 * Load margin notes script on singular tpl
 	 */
 	public function enqueue_marginotes_lib() {
-		$script_registration = wp_register_script( 'margin-notes', BEA_WPMN_URL . 'assets/js/vendor/marginotes.js', array( 'jquery' ), false, true);
-		if ( false === $script_registration ) {
+		$marginotes_registration = wp_register_script( 'margin-notes', BEA_WPMN_URL . 'assets/js/vendor/marginotes.js', array( 'jquery' ), false, true);
+		if ( false === $marginotes_registration ) {
 			trigger_error( __( 'BEA WP Margin Notes : wp_register_script fails to register assets/js/vendor/marginotes.js', 'bea-wp-margin-notes' ) );
+		}
+
+		$script_registration = wp_register_script( 'bea-wpmn', BEA_WPMN_URL . 'assets/js/script.js', array( 'jquery', 'margin-notes' ), false, true);
+		if ( false === $script_registration ) {
+			trigger_error( __( 'BEA WP Margin Notes : wp_register_script fails to register assets/js/script.js', 'bea-wp-margin-notes' ) );
 		}
 
 		// Load the script only in singular tpl (pages, posts, cpts)
 		if ( is_singular() ) {
 			wp_enqueue_script( 'margin-notes' );
+			wp_enqueue_script( 'bea-wpmn' );
 		}
 	}
 
